b1d6e760763b417ab5c46205d21daddfe01fd3a3
Marina <marina508@gmail.com>
2026-09-18 17:54:10 +0100

    Corrige estrutura e validacao de musicas

--- Musica.cs
+++ Musica.cs
@@ -10,16 +10,26 @@ namespace GestaoMusicas
             public int duracaoSegundos;
             public int ano;
 
+        // Lista de classificações
+            public List<int> classificacoes = new List<int>();
+
+        //Array de string dos gêneros musicais    
+            public string[] generos;
+            
+
         //------ CONSTRUTORES ------
         public Musica()
                 {
-                    
+                    classificacoes = new List<int>();
+                    generos = new string[3];
                 }
         public Musica (string titulo, string artista, int duracaoSegundos)
                 {
                     this.titulo = titulo;
                     this.artista = artista;
                     this.duracaoSegundos = duracaoSegundos;
+                    classificacoes = new List<int>();
+                    generos = new string[3];
                 }
         public Musica (string titulo, string artista, int duracaoSegundos, int ano)
                 {
@@ -27,7 +37,97 @@ namespace GestaoMusicas
                     this.artista = artista;
                     this.duracaoSegundos = duracaoSegundos;
                     this.ano = ano;
+                    classificacoes = new List<int>();
+                    generos = new string[3];
                 }             
-       
+        //------ MÉTODOS ------
+        
+        public void MostrarFicha()
+        {
+            Console.WriteLine("--------------");
+            Console.WriteLine("Música     : " + titulo);
+            Console.WriteLine("Artista    : " + artista);
+            Console.WriteLine("Duração    : " + duracaoSegundos);
+            Console.WriteLine("Ano        : " + ano);
+        }
+
+        public string ObterDuracaoFormatada()
+        {
+            int minutos = duracaoSegundos / 60;
+            int segundos = duracaoSegundos % 60;
+            return $"{minutos}m {segundos}s";
+        }
+
+        //------- LISTAS ----------
+        public List<int> classificacoes = new List<int>();
+
+        public void AdicionarClassificacao(int nota)
+            {
+                if (nota < 1 || nota > 5)
+                {
+                    Console.WriteLine($"Aviso: A nota {nota} é inválida! Deve ser entre 1 e 5.");
+                    return;
+                }
+                    classificacoes.Add(nota);
+            }
+        
+        public void MostrarClassificacoes()
+            {
+                if (classificacoes.Count == 0)
+                {
+                    Console.WriteLine("A lsita Classificações está vazia");
+                    return; //sai do método para não continuar
+                }
+                //Caso a lista não esteja vazia, percorre com foreach e imprime cada nota 
+                foreach(int nota in classificacoes)
+                {
+                    Console.WriteLine(nota);
+                } 
+            }
+
+        public double CalcularMedia()
+            {
+                if (classificacoes.Count == 0)
+                {
+                    return 0;
+                }
+                
+                int soma = 0;
+                
+                for (int i = 0; i < classificacoes.Count; i++)
+                {
+                    soma += classificacoes[i]; //é usado para somar cada nota, de acordo com a sequência i 
+                }
+
+                double media = (double) soma / classificacoes.Count;
+                return media;
+            }
+
+        public void MostrarGeneros()
+        {
+            Console.WriteLine("Gêneros musicais:  ");
+            bool temPreenchido = false;
+
+            //o ciclo "for" percorre o array do início ao fim usando .Length
+            for(int i = 0; i < generos.Length; i++)
+            {
+                //verificar se a posição do array não está vazia (null)
+                if(generos[i] != null)
+                {
+                    Console.WriteLine($"- {generos[i]}");
+                    temPreenchido = true;
+                }
+            }
+            if(!temPreenchido)
+            {
+                Console.WriteLine("Nenhum gênero preenchido de momento.");
+            }
+        }
+
+    public override string ToString()
+        {
+            //usar o método ObterDuracaoFormatada() que já foi criado para mostrar a duração (3m45s)
+            return ($"Título: {titulo} | Artista: {artista} | Duração {ObterDuracaoFormatada()} | Ano: {ano}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GestaoMusicas
 {
@@ -6,13 +7,31 @@ namespace GestaoMusicas
     {
         static void Main(string[] args)
         {
-            public string titulo;
-            public string artista;
-            public int duracaoSegundos;
-            public int ano;
+            // Objeto criado com o construtor vazio
+            Musica m1 = new Musica();
 
+            //Objeto criado com o construtor de 3 parâmetros
+            Musica m2 = new Musica("Drão","Gilberto Gil", 197 );
 
-            Console.WriteLine();
+            // Objeto criado com o construtor de 4 parâmetros
+            Musica m3 = new Musica("My girl", "The Temptations", 165, 1964);
+
+            m1.MostrarFicha();
+            m2.MostrarFicha();
+            m3.MostrarFicha();
+
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+
+            m2.AdicionarClassificacao(5);
+            m2.AdicionarClassificacao(2);
+            m2.AdicionarClassificacao(0);
+            m2.AdicionarClassificacao(12);
+
+            m1.ObterDuracaoFormatada();
+            m2.ObterDuracaoFormatada();
+            m3.ObterDuracaoFormatada();
         }
     }
 }
